ece6c4f4a6fecd59fa6020de0e4753a83e9f7f89
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2022-12-27 22:50:21 +0300

    Initial project setup

---
+++ ClvrtcTaskFour/Controllers/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  ClvrtcTaskFour
+//
+//  Created by Artyom Beldeiko on 27.12.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
