3ca79f26e6f894c0bab963e5826e2de2af9403ab
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2022-12-30 17:54:35 +0300

    CONFIGURED MapViewController (preliminary setup)

--- ClvrtcTaskFour/Controllers/MapViewController.swift
+++ ClvrtcTaskFour/Controllers/MapViewController.swift
@@ -6,24 +6,89 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    let locationManager = CLLocationManager()
+    
+    private let mapView: MKMapView = {
+        let view = MKMapView()
+        view.mapType = .standard
+        view.isZoomEnabled = true
+        view.isScrollEnabled = true
+        view.isPitchEnabled = true
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        configureViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
     }
-    */
+    
+    private func configureViews() {
+        view.addSubview(mapView)
+        mapView.frame = view.bounds
+    }
+    
+    private func renderLocation(_ location: CLLocation) {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude,
+                                                longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
+}
 
+// MARK: - MKMapViewDelegate
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if CLLocationManager.locationServicesEnabled() {
+            switch locationManager.authorizationStatus {
+                
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+                
+            case .authorizedAlways, .authorizedWhenInUse:
+                locationManager.requestWhenInUseAuthorization()
+                locationManager.startUpdatingLocation()
+                print("Access is allowed")
+                
+            case .denied, .restricted:
+                print("Access is denied")
+                
+            @unknown default:
+                break
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            locationManager.stopUpdatingLocation()
+            renderLocation(location)
+            
+            let currentLocationPin = MKPointAnnotation()
+            currentLocationPin.coordinate = location.coordinate
+            mapView.addAnnotation(currentLocationPin)
+        }
+    }
 }
