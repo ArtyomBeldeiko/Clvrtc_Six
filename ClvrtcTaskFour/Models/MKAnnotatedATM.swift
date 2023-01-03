b9e7d1c07a80cf47d74e05f2ddf213242abefa49
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2023-01-04 01:08:32 +0300

    DETERMINED MKAnnotatedATM model

---
+++ ClvrtcTaskFour/Models/MKAnnotatedATM.swift
@@ -0,0 +1,49 @@
+//
+//  MKAnnotatedATM.swift
+//  ClvrtcTaskFour
+//
+//  Created by Artyom Beldeiko on 3.01.23.
+//
+
+import Foundation
+import MapKit
+
+class MKAnnotatedATM: NSObject, MKAnnotation {
+    let atmID: String
+    let type: TypeEnum
+    let baseCurrency: BaseCurrency
+    let currency: Currency
+    let cards: [Card]
+    let currentStatus: CurrentStatus
+    let address: Address
+    let services: [Service]
+    let availability: Availability
+    let contactDetails: ContactDetails
+    var coordinate: CLLocationCoordinate2D
+    
+    init(atmID: String,
+         type: TypeEnum,
+         baseCurrency: BaseCurrency,
+         currency: Currency,
+         cards: [Card],
+         currentStatus: CurrentStatus,
+         address: Address,
+         services: [Service],
+         availability: Availability,
+         contactDetails: ContactDetails,
+         coordinate: CLLocationCoordinate2D) {
+        
+        self.atmID = atmID
+        self.type = type
+        self.baseCurrency = baseCurrency
+        self.currency = currency
+        self.cards = cards
+        self.currentStatus = currentStatus
+        self.address = address
+        self.services = services
+        self.availability = availability
+        self.contactDetails = contactDetails
+        self.coordinate = coordinate
+    }
+}
+
