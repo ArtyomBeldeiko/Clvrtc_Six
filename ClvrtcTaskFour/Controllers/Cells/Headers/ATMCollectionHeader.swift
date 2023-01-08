c955a26478519185edb708825c2b7cfea59c16e4
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2023-01-08 21:29:04 +0300

    ADDED ATMCellHeader

---
+++ ClvrtcTaskFour/Controllers/Cells/Headers/ATMCollectionHeader.swift
@@ -0,0 +1,32 @@
+//
+//  ATMCollectionHeader.swift
+//  ClvrtcTaskFour
+//
+//  Created by Artyom Beldeiko on 8.01.23.
+//
+
+import UIKit
+
+class ATMCollectionHeader: UICollectionReusableView {
+    
+    static let identifier = "ATMCollectionHeader"
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.1
+        return label
+    }()
+    
+    public func configure() {
+        addSubview(label)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = bounds
+    }
+}
