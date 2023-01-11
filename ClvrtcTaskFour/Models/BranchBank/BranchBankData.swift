deba7fc3c3df857f66a0a0bbf1f4c5351160e3f3
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2023-01-11 17:38:56 +0300

    REFACTORED project structure

---
+++ ClvrtcTaskFour/Models/BranchBank/BranchBankData.swift
@@ -0,0 +1,8 @@
+//
+//  BranchBankData.swift
+//  ClvrtcTaskFour
+//
+//  Created by Artyom Beldeiko on 11.01.23.
+//
+
+import Foundation
---
+++ ClvrtcTaskFour/Models/ServiceTerminal/ServiceTerminal.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceTerminal.swift
+//  ClvrtcTaskFour
+//
+//  Created by Artyom Beldeiko on 10.01.23.
+//
+
+import Foundation
