51d36c68f7a3fc16d455cfdc23c34cebea51b46a
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2023-01-18 01:59:45 +0300

    FIXED data representation issue: rewrite branchBankDatesFormatter

--- ClvrtcTaskFour/Helpers/DatesFormatter.swift
+++ ClvrtcTaskFour/Helpers/DatesFormatter.swift
@@ -56,26 +56,61 @@ func branchBankDatesFormatter(_ days: [BankDayElement]) -> String {
         
     for item in days {
         if item.dayCode == 01 {
-            monday = "Пн: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
-            output += monday
+            if item.openingTime.rangeOfCharacter(from: .decimalDigits) != nil && item.closingTime.rangeOfCharacter(from: .decimalDigits) != nil {
+                monday = "Пн: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
+                output += monday
+            } else {
+                monday = "Пн: выходной; "
+                output += monday
+            }
         } else if item.dayCode == 02 {
-            tuesday = "Вт: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
-            output += tuesday
+            if item.openingTime.rangeOfCharacter(from: .decimalDigits) != nil && item.closingTime.rangeOfCharacter(from: .decimalDigits) != nil {
+                tuesday = "Вт: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
+                output += tuesday
+            } else {
+                tuesday = "Вт: выходной; "
+                output += tuesday
+            }
         } else if item.dayCode == 03 {
-            wednesday = "Cр: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
-            output += wednesday
+            if item.openingTime.rangeOfCharacter(from: .decimalDigits) != nil && item.closingTime.rangeOfCharacter(from: .decimalDigits) != nil {
+                wednesday = "Cр: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
+                output += wednesday
+            } else {
+                wednesday = "Cр: выходной; "
+                output += wednesday
+            }
         } else if item.dayCode == 04 {
-            thursday = "Чт: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
-            output += thursday
+            if item.openingTime.rangeOfCharacter(from: .decimalDigits) != nil && item.closingTime.rangeOfCharacter(from: .decimalDigits) != nil {
+                thursday = "Чт: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
+                output += thursday
+            } else {
+                thursday = "Чт: выходной; "
+                output += thursday
+            }
         } else if item.dayCode == 05 {
-            friday = "Пт: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3)))"
-            output += friday
+            if item.openingTime.rangeOfCharacter(from: .decimalDigits) != nil && item.closingTime.rangeOfCharacter(from: .decimalDigits) != nil {
+                friday = "Пт: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3))); "
+                output += friday
+            } else {
+                friday = "Пт: выходной; "
+                output += friday
+            }
         } else if item.dayCode == 06 {
-            saturday = "Cб: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3)))"
-            output += "; " + saturday
+            if item.openingTime.rangeOfCharacter(from: .decimalDigits) != nil && item.closingTime.rangeOfCharacter(from: .decimalDigits) != nil {
+                saturday = "Cб: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3)))"
+                output += "; " + saturday
+            } else {
+                saturday = "Cб: выходной"
+                output += "; " + saturday
+            }
         } else if item.dayCode == 07 {
-            sunday = "Вс: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3)))"
-            output += "; " + sunday
+            if item.openingTime.rangeOfCharacter(from: .decimalDigits) != nil && item.closingTime.rangeOfCharacter(from: .decimalDigits) != nil {
+                sunday = "Вс: \(item.openingTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.closingTime.components(separatedBy: "+")[0].dropLast(3)) (перерыв: \(item.dayBreak.breakFromTime.components(separatedBy: "+")[0].dropLast(3)) - \(item.dayBreak.breakToTime.components(separatedBy: "+")[0].dropLast(3)))"
+                output += "; " + sunday
+            } else {
+                sunday = "Вс: выходной"
+                output += "; " + sunday
+            }
         }
     }
     return output
