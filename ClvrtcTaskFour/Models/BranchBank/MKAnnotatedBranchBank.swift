bad5d32500150ee9fce2654ef10fbdf2584ae5d7
Artyom Beldeiko <artyom.beldeiko@gmail.com>
2023-01-18 01:34:55 +0300

    REDESIGNED MKAnnotatedBranchBank model to primitive types

--- ClvrtcTaskFour/Models/BranchBank/MKAnnotatedBranchBank.swift
+++ ClvrtcTaskFour/Models/BranchBank/MKAnnotatedBranchBank.swift
@@ -9,42 +9,61 @@ import Foundation
 import MapKit
 
 final class MKAnnotatedBranchBank: NSObject, MKAnnotation {
-    let branchID, name, cbu: String
-    let accountNumber: String
-    let equeue, wifi: Int
-    let accessibilities: BankAccessibilities
-    let branchBankAddress: BankAddress
-    let information: BankInformation
-    let services: BankServices
-    let coordinate: CLLocationCoordinate2D
+    let branchID: String
+    let name: String
+    let cbu: String
+    let equeue: Int
+    let wifi: Int
+    let streetName: String
+    let buildingNumber: String
+    let department: String
+    let townName: String
+    let addressLine: String
+    let addressDescription: String
+    let latitude: String
+    let longitude: String
+    let standardAvailability: String
+    let currency: String
+    
+    var coordinate : CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: Double(latitude)!, longitude: Double(longitude)!)
+    }
     
     var location: CLLocation {
-        return CLLocation(latitude: Double(branchBankAddress.geoLocation.geographicCoordinates.latitude)!, longitude: Double(branchBankAddress.geoLocation.geographicCoordinates.latitude)!)
+        return CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
     }
     
     init(branchID: String,
          name: String,
          cbu: String,
-         accountNumber: String,
          equeue: Int,
          wifi: Int,
-         accessibilities: BankAccessibilities,
-         branchBankAddress: BankAddress,
-         information: BankInformation,
-         services: BankServices,
-         coordinate: CLLocationCoordinate2D) {
+         streetName: String,
+         buildingNumber: String,
+         department: String,
+         townName: String,
+         addressLine: String,
+         addressDescription: String,
+         latitude: String,
+         longitude: String,
+         standardAvailability: String,
+         currency: String) {
         
         self.branchID = branchID
         self.name = name
         self.cbu = cbu
-        self.accountNumber = accountNumber
         self.equeue = equeue
         self.wifi = wifi
-        self.accessibilities = accessibilities
-        self.branchBankAddress = branchBankAddress
-        self.information = information
-        self.services = services
-        self.coordinate = coordinate
+        self.streetName = streetName
+        self.buildingNumber = buildingNumber
+        self.department = department
+        self.townName = townName
+        self.addressLine = addressLine
+        self.addressDescription = addressDescription
+        self.latitude = latitude
+        self.longitude = longitude
+        self.standardAvailability = standardAvailability
+        self.currency = currency
     }
     
     func distance(to location: CLLocation) -> CLLocationDistance {
